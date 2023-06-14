95209e920ed87975dc7be5fbe2193b318cdb29e0
Edison Rojas <edison.rojasdev@gmail.com>
2023-06-14 14:07:35 -0500

    feat: Add icon library to the project and configure it using environment variables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-
+import {NgIconsModule,} from '@ng-icons/core';
+import { heroUsers } from '@ng-icons/heroicons/outline';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgIconsModule.withIcons({heroUsers})
   ],
   providers: [],
   bootstrap: [AppComponent]
